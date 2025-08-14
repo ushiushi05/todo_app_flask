eaee389b3701455efcf95a053ec58a2f0ff733d3
ushiushi05 <ushin1211@gmail.com>
2025-08-14 11:39:36 +0900

    first commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_templete, request, redirect
+from flask import Flask, render_template, request, redirect
 import sqlite3
 
 app = Flask(__name__)
@@ -17,4 +17,28 @@ def index():
     c.execute("SELECT * FROM tasks")
     tasks = c.fetchall()
     conn.close()
-    return render_templete("index.html", tasks=tasks)
+    return render_template("index.html", tasks=tasks)
+
+@app.route("/add", method=["POST"])
+def add():
+    task = request.form["task"]
+    conn = sqlite3.connect("todo.db")
+    c = conn.cursor()
+    c.execute("INSERT INTO tasks(task) VALUES(?)", (task,))
+    conn.commit()
+    conn.close()
+    return redirect("/")
+
+@app.route("/delete.<int:id>")
+def delete(id):
+    conn = sqlite3.connect("todo.db")
+    c = conn.cursor()
+    c.execute("DELETE FROM tasks WHERE id=?", (id,))
+    conn.commit()
+    conn.close()
+    return redirect("/")
+
+if __name__ == "__main__":
+    init_db()
+    app.run(debug=True)
+    
